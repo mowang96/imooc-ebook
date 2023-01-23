aeceafcbd54144f980041a2021fe81c8eb73acd8
mowang96 <enjied1988@outlook.com>
2023-01-24 03:29:52 +0800

    day1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="read"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Epub from 'epubjs'
 
+global.Epub = Epub
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  mounted() {
+    this.book = new Epub('/test.epub')
+    console.log(this.book)
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
   }
 }
 </script>
