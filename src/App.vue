0ff271e0d50c176bdb20348d99e40c2652e7d702
mowang96 <enjied1988@outlook.com>
2023-01-24 23:46:30 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template>
   <div id="app">
+    <span class="text">ABCDEFG</span>
     <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['test'])
+    },
+    mounted() {
+      this.$store.dispatch('setTest', 1).then(() => {
+        console.log(this.test)
+      })
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+    console.log(document.querySelector('html'))
+  })
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+  .text {
+    font-family: 'Days One';
+    font-size: px2rem(20);
+    color: orange;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="home">
-    <div id="read"></div>
+    <!-- <div id="read"></div> -->
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
   </div>
 </template>
 
 <script>
-import Epub from 'epubjs'
+// import Epub from 'epubjs'
 
-global.Epub = Epub
-export default {
-  name: 'home',
-  mounted() {
-    this.book = new Epub('/test.epub')
-    console.log(this.book)
-    this.book.renderTo('read', {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }).display()
-  }
-}
+// global.Epub = Epub
+// export default {
+//   name: 'home',
+//   mounted () {
+//     this.book = new Epub('/test.epub')
+//     console.log(this.book)
+//     this.book.renderTo('read', {
+//       width: window.innerWidth,
+//       height: window.innerHeight
+//     }).display()
+//   }
+// }
 </script>
+
+<style scoped>
+.icon-clock {
+  font-size: 30px;
+  color: red;
+}
+</style>
